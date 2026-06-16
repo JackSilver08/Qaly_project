8968f6b46ff1cfd013ecde791a473ca38d588310
Tuan Tran Quang <tuantqtb01555@example.com>
2026-06-16 10:53:13 +0700

    Update database

--- src/Qaly.Infrastructure/Data/Migrations/20260517084404_AddTaskTimelineAttention.cs
+++ src/Qaly.Infrastructure/Data/Migrations/20260517084404_AddTaskTimelineAttention.cs
@@ -13,6 +13,41 @@ namespace Qaly.Infrastructure.Data.Migrations
         {
             migrationBuilder.Sql(
                 """
+                IF OBJECT_ID(N'[ImportSessions]', N'U') IS NULL
+                BEGIN
+                    CREATE TABLE [ImportSessions] (
+                        [Id] uniqueidentifier NOT NULL CONSTRAINT [PK_ImportSessions] PRIMARY KEY DEFAULT NEWID(),
+                        [ProjectId] uniqueidentifier NOT NULL,
+                        [UserId] uniqueidentifier NOT NULL,
+                        [FileName] nvarchar(256) NOT NULL,
+                        [TotalRows] int NOT NULL CONSTRAINT [DF_ImportSessions_TotalRows] DEFAULT 0,
+                        [ImportedCount] int NOT NULL CONSTRAINT [DF_ImportSessions_ImportedCount] DEFAULT 0,
+                        [SkippedCount] int NOT NULL CONSTRAINT [DF_ImportSessions_SkippedCount] DEFAULT 0,
+                        [IsUndone] bit NOT NULL CONSTRAINT [DF_ImportSessions_IsUndone] DEFAULT 0,
+                        [CreatedAt] datetimeoffset NOT NULL CONSTRAINT [DF_ImportSessions_CreatedAt] DEFAULT SYSDATETIMEOFFSET(),
+                        [UpdatedAt] datetimeoffset NULL
+                    );
+                END;
+
+                IF OBJECT_ID(N'[TaskAssignments]', N'U') IS NULL
+                BEGIN
+                    CREATE TABLE [TaskAssignments] (
+                        [Id] uniqueidentifier NOT NULL CONSTRAINT [PK_TaskAssignments] PRIMARY KEY DEFAULT NEWID(),
+                        [TaskItemId] uniqueidentifier NOT NULL,
+                        [UserId] uniqueidentifier NOT NULL,
+                        [CreatedAt] datetimeoffset NOT NULL CONSTRAINT [DF_TaskAssignments_CreatedAt] DEFAULT SYSDATETIMEOFFSET(),
+                        [UpdatedAt] datetimeoffset NULL
+                    );
+                END;
+
+                IF COL_LENGTH(N'[TaskItems]', N'ImportSessionId') IS NULL
+                BEGIN
+                    ALTER TABLE [TaskItems] ADD [ImportSessionId] uniqueidentifier NULL;
+                END;
+                """);
+
+            migrationBuilder.Sql(
+                """
                 IF EXISTS (
                     SELECT 1
                     FROM sys.foreign_keys
